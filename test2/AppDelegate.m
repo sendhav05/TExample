ae79c6107beb3eebe32ab1831b9746cd217d5fbc
Narendra <sendhav05@Vodafone.local>
2016-12-27 22:44:08 +0530

    feature mesaage 1

--- test2/AppDelegate.m
+++ test2/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //feature 1 brnach
     return YES;
 }
 
